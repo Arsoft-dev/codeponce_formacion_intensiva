c471aa0e5236a9e994a56ba00bc74a00b96dd006
Abraham Ruiz <abrahanruiz1@gmail.com>
2026-07-16 20:36:14 -0400

    feat: crear primera página HTML

---
+++ clase_03/script.js
@@ -0,0 +1,4 @@
+document.getElementById('btnSaludar').addEventListener('click', function () {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.textContent = '¡Hola! Gracias por hacer clic. 🎉';
+});
