e2757fdce73d4a3a6f866c14077188f2b871ef45
Abraham Ruiz <abrahanruiz1@gmail.com>
2026-07-16 21:07:24 -0400

    style: se agrego un boton para alternar entre modos obscuros y claros

--- clase_03/script.js
+++ clase_03/script.js
@@ -2,3 +2,8 @@ document.getElementById('btnSaludar').addEventListener('click', function () {
   const mensaje = document.getElementById('mensaje');
   mensaje.textContent = '¡Hola! Gracias por hacer clic. 🎉';
 });
+
+document.getElementById('btnDarkMode').addEventListener('click', function () {
+  document.body.classList.toggle('dark');
+  this.textContent = document.body.classList.contains('dark') ? '☀️ Modo claro' : '🌙 Modo oscuro';
+});
